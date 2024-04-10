a8963554f8b92cf7a55b8a201f57e475cc3de8ef
xiaoka <panjixiao@outlook.com>
2024-04-10 15:18:12 +0900

    将if结构重写为switch结构

--- 3/22/index.js
+++ 3/22/index.js
@@ -1,14 +1,15 @@
-let browser;
+let a = +prompt('a?', '');
+// 这里这个加号是为了将输入的字符串类型的数字转为数字类型
 
-if (browser === 'Edge') {
-  alert('You have got the Edge!');
-} else if (
-  browser === 'Chrome' ||
-  browser === 'Firefox' ||
-  browser === 'Safari' ||
-  browser === 'Opera'
-) {
-  alert('Okay we support that too');
-} else {
-  alert('We hope that this page looks ok!');
+switch (a) {
+  case 0:
+    alert(0);
+    break;
+  case 1:
+    alert(1);
+    break;
+  case 2:
+  case 3:
+    alert(0);
+    break;
 }
