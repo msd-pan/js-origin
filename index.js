fe2844f3fd3cd87ef4c4c18264061387f3caefe1
xiaoka <panjixiao@outlook.com>
2024-04-15 16:39:18 +0900

    创建new Accumulator

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-function Calculator() {
+function Accumulator(startingValue) {
+  this.value = startingValue;
+
   this.read = function () {
-    let a = +prompt('a?', 0);
-    let b = +prompt('b?', 0);
-    this.a = a;
-    this.b = b;
-    // 给对象赋属性可以用以下语句
-    // this.a = +prompt('a?', 0);
-    // this.b = +prompt('b?', 0);
-  };
-
-  this.sum = function () {
-    return this.a + this.b;
-  };
-
-  this.mul = function () {
-    return this.a * this.b;
+    this.value += +prompt('enter a number', 0);
   };
 }
-let calculator = new Calculator();
-calculator.read();
-alert('Sum=' + calculator.sum());
-alert('Mul=' + calculator.mul());
+
+let accumulator = new Accumulator(10);
+accumulator.read();
+accumulator.read();
+
+alert(accumulator.value);
