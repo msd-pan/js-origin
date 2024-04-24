bcfdd7dd87ee38aab22cb5bcd2400d28515b51a7
xiaoka <panjixiao@outlook.com>
2024-04-24 10:27:01 +0900

    数字类型：来自访问者的数字的总和

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-function Accumulator(startingValue) {
-  this.value = startingValue;
+let a = +prompt('first number?', '');
+let b = +prompt('second number?', '');
 
-  this.read = function () {
-    this.value += +prompt('enter a number', 0);
-  };
-}
-
-let accumulator = new Accumulator(10);
-accumulator.read();
-accumulator.read();
-
-alert(accumulator.value);
+alert(a + b);
