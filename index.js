b92d21dc0b7ee831a087f7360e27ad62e86e509c
xiaoka <panjixiao@outlook.com>
2024-04-24 11:51:23 +0900

    从min到max的随机数

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-alert((0.2).toFixed(20));
+let random = (min, max) => {
+  let ranNum;
+  do {
+    ranNum = (max - min) * Math.random() + min;
+  } while (ranNum >= max || ranNum < min);
+  return ranNum;
+};
 
-let i = 0;
-while (i < 11) {
-  i += 0.2;
-  if (i > 9.8 && i < 10.2) alert(i);
-}
+/* 我们需要将区间0到1中的所有值“映射”为范围在min到max中的值。
+    可以分为两个阶段完成：
+    1：如果我们将0到1的随机数乘以max-min，则随机数的范围将从0到1增加到0到max-min
+    2：现在，如果我们将随机数与min相加，则随机数的范围将为min到max。
+*/
 
-// i永远不会等于10. 之所以发生这种情况，是因为对0.2这样的小数时进行加法运算时出现了精度损失。
-// 结论：在处理小数时避免相等性检查。
+/* 答案函数实现
+function random(min, max) {
+  return;
+  min + Math.random() * (max - min);
+} */
+alert(random(1, 5));
+alert(random(1, 5));
+alert(random(1, 5));
