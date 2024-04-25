9c5401b5b5f826aa687a18b93858d68c674c8f29
xiaoka <panjixiao@outlook.com>
2024-04-25 10:37:52 +0900

    检查spam

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let ucFirst = (str) => {
-  if (!str) return str;
-  str = str[0].toUpperCase() + str.slice(1);
-  alert(str);
+let checkSpam = (str) => {
+  if (
+    str.toLowerCase().indexOf('viagra') != -1 ||
+    str.toLowerCase().indexOf('xxx') != -1
+  ) {
+    return true;
+  } else return false;
 };
 
-ucFirst('like');
-
-/* 解释
-在js中字符串是不可变的，所以我们可以根据已有字符串创建一个首字母大写的新字符串。
-同时，如股票str是空的，那么str[0]就是undefined，但由于undefined并没有toUpperCase()方法，因此会得到一个错误。所以当检测到为空字符串时，返回空字符串
-*/
+alert(checkSpam('buy ViAgRA now'));
+alert(checkSpam('free xxxxx'));
+alert(checkSpam('innocent rabbit'));
