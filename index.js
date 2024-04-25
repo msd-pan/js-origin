c09fd899c6d7b312b7976f4b88f75de1d4c00fa6
xiaoka <panjixiao@outlook.com>
2024-04-25 13:44:08 +0900

    数组的循环：使用for of与for in的区别

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 let arr = [1, 2, 3, 4, 5];
-alert(arr.at(-3));
+
+// 使用of关键字的时候它用于迭代可迭代对象（例如数组），迭代的是数组的值
+for (let i of arr) {
+  alert(i);
+}
+
+// 使用of关键字的时候它用于迭代对象的属性，迭代的是数组的索引
+for (let i in arr) {
+  alert(i);
+}
