09848e2ec18d7819878cacf3f39d000b438bae4f
xiaoka <panjixiao@outlook.com>
2024-04-25 14:46:43 +0900

    输入数字求和

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-let styles = ['Jazz', 'Blues'];
+// 自己做成，有错误
 
-styles.push('Rock-n-Roll');
-styles[Math.floor((styles.length - 1) / 2)] = 'Classics';
-alert(styles.shift());
-styles.unshift('Rap', 'Reggae');
+// let sumInput = () => {
+//   let arr = [];
+//   let total;
+//   do {
+//     let num;
+//     num = prompt('enter a number?', 0);
+//     if (num !== null && num !== '') {
+//       // 检查用户是否取消输入
+//       arr.push(Number(num)); // 将输入值转换为数字类型
+//     }
+//   } while (num !== null && num !== '');
+//   for (let num of arr) {
+//     total += num;
+//   }
+//   return total;
+// };
+
+// 答案
+function sumInput() {
+  let numbers = [];
+  while (true) {
+    let value = prompt('A number please?', 0);
+    // 应该结束了吗？
+    if (value === '' || value === null || !isFinite(value)) break;
+    numbers.push(+value);
+  }
+  let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum;
+}
+
+alert(sumInput());
