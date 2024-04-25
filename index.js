f7c4ee4791c60d44c40a0fb487d495ceb868d6e1
xiaoka <panjixiao@outlook.com>
2024-04-25 11:08:31 +0900

    提取货币

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-let truncate = (str, maxLength) => {
-  if (str.length > maxLength) {
-    let newStr = str.slice(0, maxLength - 1) + '...';
-    return newStr;
-  } else {
-    return str;
-  }
+let extractCurrencyValue = (str) => {
+  str = +str.slice(1);
+  return str;
 };
 
-alert(truncate('Hi Joshua!', 20));
-alert(truncate('I miss you and want hug you when we meet!', 20));
-
-// 省略号实际上有一个单独的Unicode字符，而不是三个点。
+alert(extractCurrencyValue('$120') === 120);
