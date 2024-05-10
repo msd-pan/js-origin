72e36d5f5bc43c2cc6ab9d08ab522c6004ffae59
xiaoka <panjixiao@outlook.com>
2024-05-10 10:39:05 +0900

    Map 还可以使用对象作为键。使用对象作为键是 Map 最值得注意和重要的功能之一。

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-let map = new Map();
+let john = { name: 'John' };
 
-map.set('1', 'str1'); //字符串键
-map.set(1, 'num1'); //数字键
-map.set(true, 'bool1'); //布尔值键
+// 存储每个用户的来访次数
+let visitsCountMap = new Map();
 
-// 普通的Object对象会将键转化为字符串
-// Map则会保留键的类型，所以下面这两个结果不同：
-alert(map.get(1)); // 'num1'
-alert(map.get('1')); // 'str1'
-alert(map.size); // 3
+// john事Map中的键
+visitsCountMap.set(john, 123);
 
-//Map 是一个带键的数据项的集合，就像一个 Object 一样。 但是它们最大的差别是 Map 允许任何类型的键（key）。
+alert(visitsCountMap.get(john)); // 123
