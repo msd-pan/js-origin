6101bbef9270a10d296f3487ad1c714884646f0d
xiaoka <panjixiao@outlook.com>
2024-05-10 10:16:56 +0900

    Array.from: 用于将可迭代对象或类数组对象转换为对象的方法

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-let str = 'hello';
+let arrayLike = {
+  0: 'Hello',
+  1: 'World',
+  length: 2,
+};
 
-// 和for..of做同样的事
-// for (let char of str)  alert(char)
-let iterator = str[Symbol.iterator]();
+//  Array.from 方法接受对象，检查它是一个可迭代对象或类数组对象，然后创建一个新数组，并将该对象的所有元素复制到这个新数组。
+let arr = Array.from(arrayLike);
+alert(arr.pop());
+alert(arr.pop());
 
-while (true) {
-  let result = iterator.next();
-  if (result.done) break;
-  alert(result.value); //一个接一个的输出字符
-}
+// Array.from 的完整语法
+// Array.from(obj[, mapFn, thisArg])
+
+// 可选的第二个参数 mapFn 可以是一个函数，该函数会在对象中的元素被添加到数组前，被应用于每个元素，此外 thisArg 允许我们为该函数设置 this。
+
+// 求每个数的平方
+
+let range = {
+  from: 1,
+  to: 5,
+};
+
+range[Symbol.iterator] = function () {
+  return {
+    current: this.from,
+    last: this.to,
+
+    next() {
+      if (this.current <= this.last) {
+        return { done: false, value: this.current++ };
+      } else {
+        return { done: true };
+      }
+    },
+  };
+};
+
+let newArr = Array.from(range, (num) => num * num);
+
+alert(newArr); // 1,4,9,16,25
