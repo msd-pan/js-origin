a3bc82e7667cd75331e9332bd7a97719c39af38e
xiaoka <panjixiao@outlook.com>
2024-05-10 10:35:19 +0900

    Map and Set（映射和集合）:Map初始例子

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
-let arrayLike = {
-  0: 'Hello',
-  1: 'World',
-  length: 2,
-};
+let map = new Map();
 
-//  Array.from 方法接受对象，检查它是一个可迭代对象或类数组对象，然后创建一个新数组，并将该对象的所有元素复制到这个新数组。
-let arr = Array.from(arrayLike);
-alert(arr.pop());
-alert(arr.pop());
+map.set('1', 'str1'); //字符串键
+map.set(1, 'num1'); //数字键
+map.set(true, 'bool1'); //布尔值键
 
-// Array.from 的完整语法
-// Array.from(obj[, mapFn, thisArg])
+// 普通的Object对象会将键转化为字符串
+// Map则会保留键的类型，所以下面这两个结果不同：
+alert(map.get(1)); // 'num1'
+alert(map.get('1')); // 'str1'
+alert(map.size); // 3
 
-// 可选的第二个参数 mapFn 可以是一个函数，该函数会在对象中的元素被添加到数组前，被应用于每个元素，此外 thisArg 允许我们为该函数设置 this。
-
-// 求每个数的平方
-
-let range = {
-  from: 1,
-  to: 5,
-};
-
-range[Symbol.iterator] = function () {
-  return {
-    current: this.from,
-    last: this.to,
-
-    next() {
-      if (this.current <= this.last) {
-        return { done: false, value: this.current++ };
-      } else {
-        return { done: true };
-      }
-    },
-  };
-};
-
-let newArr = Array.from(range, (num) => num * num);
-
-alert(newArr); // 1,4,9,16,25
+//Map 是一个带键的数据项的集合，就像一个 Object 一样。 但是它们最大的差别是 Map 允许任何类型的键（key）。
