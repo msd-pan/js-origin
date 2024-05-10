9f52a95bba7adc977fa1c2bbdf9a6fdf0dd6dd80
xiaoka <panjixiao@outlook.com>
2024-05-10 11:30:44 +0900

    Set

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-let map = new Map();
-map.set('banana', 1);
-map.set('orange', 2);
-map.set('meat', 4);
+let set = new Set();
 
-let obj = Object.fromEntries(map.entries());
-// 创建一个普通对象(plain object)
+let john = { name: 'john' };
+let pete = { name: 'pete' };
+let mary = { name: 'mary' };
 
-// 完成了
-// obj = { banana: 1, orange: 2, meat: 4 }
-alert(obj.orange);
+// visits, 一些访客来访好几次
+set.add(john);
+set.add(pete);
+set.add(mary);
+set.add(john);
+set.add(mary);
 
-// Object.fromEntries 方法的作用是相反的：给定一个具有 [key, value] 键值对的数组，它会根据给定数组创建一个对象：
+// set 只保留不重复的值
+alert(set.size);
+
+for (let user of set) {
+  alert(user.name);
+}
+
+/* Set 是一个特殊的类型集合 —— “值的集合”（没有键），它的每一个值只能出现一次。
+
+它的主要特点是，重复使用同一个值调用 set.add(value) 并不会发生什么改变。这就是 Set 里面的每一个值只出现一次的原因。
+
+例如，我们有客人来访，我们想记住他们每一个人。但是已经来访过的客人再次来访，不应造成重复记录。每个访客必须只被“计数”一次。
+*/
