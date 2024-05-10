73f7e81cf0d0cbc704815a717dbf4e9b40b16ce9
xiaoka <panjixiao@outlook.com>
2024-05-10 10:50:19 +0900

    Map 迭代

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-let john = { name: 'John' };
+let recipeMap = new Map([
+  ['cucumber', 500],
+  ['tomatoes', 350],
+  ['onion', 50],
+]);
 
-// 存储每个用户的来访次数
-let visitsCountMap = new Map();
+// 遍历所有的键
+for (let vegetable of recipeMap.keys()) {
+  alert(vegetable);
+}
 
-// john事Map中的键
-visitsCountMap.set(john, 123);
+// 遍历所有的值
+for (let amount of recipeMap.values()) {
+  alert(amount);
+}
 
-alert(visitsCountMap.get(john)); // 123
+// 遍历所有的实体[key, value]
+for (let entry of recipeMap.entries()) {
+  // 与let entry of recipeMap相同
+  alert(entry);
+}
+
+/* 如果要在 map 里使用循环，可以使用以下三个方法：
+
+map.keys() —— 遍历并返回一个包含所有键的可迭代对象，
+map.values() —— 遍历并返回一个包含所有值的可迭代对象，
+map.entries() —— 遍历并返回一个包含所有实体 [key, value] 的可迭代对象，for..of 在默认情况下使用的就是这个。 */
