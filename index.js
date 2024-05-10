956003cd5ec39930130b668b906fba38dde48525
xiaoka <panjixiao@outlook.com>
2024-05-10 11:20:56 +0900

    Object.fromEntries：从 Map 创建对象

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-let obj = {
-  name: 'john',
-  age: 30,
-};
+let map = new Map();
+map.set('banana', 1);
+map.set('orange', 2);
+map.set('meat', 4);
 
-let map = new Map(Object.entries(obj));
+let obj = Object.fromEntries(map.entries());
+// 创建一个普通对象(plain object)
 
-alert(map.get('name'));
+// 完成了
+// obj = { banana: 1, orange: 2, meat: 4 }
+alert(obj.orange);
 
-/* 如果我们想从一个已有的普通对象（plain object）来创建一个 Map，那么我们可以使用内建方法 Object.entries(obj)，该方法返回对象的键/值对数组，该数组格式完全按照 Map 所需的格式。
-
-这里，Object.entries 返回键/值对数组：[ ["name","John"], ["age", 30] ]。这就是 Map 所需要的格式。
- */
+// Object.fromEntries 方法的作用是相反的：给定一个具有 [key, value] 键值对的数组，它会根据给定数组创建一个对象：
