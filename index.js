b03814706e5b873fe94438268ee1ea68bb5e1f7e
xiaoka <panjixiao@outlook.com>
2024-05-14 17:16:34 +0900

    （练习）日期与时间：创建日期

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-let ms = Date.parse('2012-01-26T13:51:50.417-07:00');
-alert(ms);
-
-let date = new Date(Date.parse('2012-01-26T13:51:50.417-07:00'));
+let date = new Date(Date.parse('2012-02-20T03:12:00'));
 alert(date);
 
-/* Date.parse(str) 方法可以从一个字符串中读取日期。
+// 这是一个以数字作为日期参数的示例：
+// new Date(year, month, date, hour, minute, second, millisecond)
+let d1 = new Date(2012, 1, 20, 3, 12);
+alert(d1);
 
-字符串的格式应该为：YYYY-MM-DDTHH:mm:ss.sssZ */
+// 我们还可以从字符串创建日期
+// new Date(datestring)
+let d2 = new Date('2012-02-20T03:12');
+alert(d2);
+
+/* new Date 构造函数默认使用本地时区。所以唯一需要牢记的就是月份从 0 开始计数。
+
+所以二月对应的数值是 1。 */
