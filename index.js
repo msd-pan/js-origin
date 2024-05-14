0c49e5d1d8901c78eb82da7637c1bea6673d3681
xiaoka <panjixiao@outlook.com>
2024-05-14 16:28:58 +0900

    基准测试（Benchmarking）

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-let start = Date.now(); // 从 1 Jan 1970 至今的时间戳
+let diffSubtract = (date1, date2) => date2 - date1;
+let diffGetTime = (date1, date2) => date2.getTime() - date1.getTime();
 
-// do the job
-for (let i = 0; i < 100000; i++) {
-  let doSomething = i * i * i;
-}
+let bench = (f) => {
+  let date1 = new Date(0);
+  let date2 = new Date();
 
-let end = Date.now(); // 完成时刻
+  let start = Date.now();
+  for (let i = 0; i < 100000; i++) f(date1, date2);
+  return Date.now() - start;
+};
 
-alert(`The loop took ${end - start} ms`); // 相减的是时间戳，而不是日期
+alert(`Time of diffSubtract: ` + bench(diffSubtract) + 'ms');
+alert(`Time of diffGetTime: ` + bench(diffGetTime) + 'ms');
 
-/*  
-关于时间戳是什么
+/* 
+看起来使用 getTime() 这种方式快得多，这是因为它没有进行类型转换，对引擎优化来说更加简单。
 
-时间戳（Timestamp）是指记录某一特定事件发生的精确时间的数值。它通常以日期和时间的形式表示，用于标记数据的时间属性。时间戳在计算机科学、数据库管理、网络通信等多个领域中被广泛使用。
+我们得到了结论，但是这并不是一个很好的度量的例子。
 
-这里的例子中的时间戳的形式是：UNIX时间戳（自1970年1月1日以来的秒数，如：1672531200）
+想象一下当运行 bench(diffSubtract) 的同时，CPU 还在并行处理其他事务，并且这也会占用资源。然而，运行 bench(diffGetTime) 的时候，并行处理的事务完成了。
+
+对于现代多进程操作系统来说，这是一个非常常见的场景。
+
+比起第二个函数，第一个函数所能使用的 CPU 资源更少。这可能导致错误的结论。
+
+为了得到更加可靠的度量，整个度量测试包应该重新运行多次。
 */
