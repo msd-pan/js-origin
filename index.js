ce01122028fb5944bb368b3a275128e2321f52d9
xiaoka <panjixiao@outlook.com>
2024-05-14 14:26:37 +0900

    日期和时间：访问日期组件

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-let set = new Set(['oranges', 'bananas', 'apples']);
+// 当前日期
+let date = new Date();
 
-for (let value of set) alert(value);
+// 当前时区的小时数
+alert(date.getHours());
 
-// 与forEach相同：
-set.forEach((value, valueAgain, set) => {
-  alert(value);
-});
-
-/*  我们可以使用 for..of 或 forEach 来遍历 Set：
-
-注意一件有趣的事儿。forEach 的回调函数有三个参数：一个 value，然后是 同一个值 valueAgain，最后是目标对象。没错，同一个值在参数里出现了两次。
-
-forEach 的回调函数有三个参数，是为了与 Map 兼容。当然，这看起来确实有些奇怪。但是这对在特定情况下轻松地用 Set 代替 Map 很有帮助，反之亦然。
-
-Map 中用于迭代的方法在 Set 中也同样支持：
-
-set.keys() —— 遍历并返回一个包含所有值的可迭代对象，
-set.values() —— 与 set.keys() 作用相同，这是为了兼容 Map，
-set.entries() —— 遍历并返回一个包含所有的实体 [value, value] 的可迭代对象，它的存在也是为了兼容 Map。
-
-
-
-*/
+// 在UTC+0 时区的小时数（非夏令时的伦敦时间）
+alert(date.getUTCHours());
