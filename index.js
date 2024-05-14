92cb90f3132863813f2cb71c6358b3ec406c48dd
xiaoka <panjixiao@outlook.com>
2024-05-14 15:04:28 +0900

    设置日期组件

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// 当前日期
-let date = new Date();
+let today = new Date();
 
-// 当前时区的小时数
-alert(date.getHours());
+today.setHours(0);
+alert(today); // 日期仍然是今天，但是只是小时数被改成了0
 
-// 在UTC+0 时区的小时数（非夏令时的伦敦时间）
-alert(date.getUTCHours());
+today.setHours(0, 0, 0, 0);
+alert(today); // 日期仍然是今天，时间为 00:00:00
