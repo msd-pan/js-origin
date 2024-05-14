4b3c06eea7601e68bca9edf6542ebdc72949200b
xiaoka <panjixiao@outlook.com>
2024-05-14 17:35:33 +0900

    (练习)欧洲的星期表示方法

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-let getWeekDay = (date) => {
-  let days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
-
-  return days[date.getDay()];
+let getLocalDay = (date) => {
+  return date.getDay() === 0 ? 7 : date.getDay();
 };
 
-let date = new Date(2014, 0, 3);
-alert(getWeekDay(date));
-
-/* getDay()
-获取一周中的第几天，从 0（星期日）到 6（星期六）。第一天始终是星期日，在某些国家可能不是这样的习惯，但是这不能被改变。 */
+let date = new Date(2012, 0, 3);
+alert(getLocalDay(date));
