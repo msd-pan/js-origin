ce46cb7a1717ef8066f587c66eabe97649d4c5fb
xiaoka <panjixiao@outlook.com>
2024-05-15 09:20:20 +0900

    JSON方法，toJSON：JSON.stringify

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-let getDateAgo = (date, days) => {
-  let tempDate = new Date(date);
-  tempDate.setDate(tempDate.getDate() + days);
-  return tempDate;
+let student = {
+  name: 'John',
+  age: 30,
+  isAdmin: false,
+  courses: ['html', 'css', 'js'],
+  spouse: null,
 };
 
-let date = new Date(2015, 0, 2);
+let json = JSON.stringify(student);
 
-alert(getDateAgo(date, 1));
-alert(getDateAgo(date, 2));
-alert(getDateAgo(date, 365));
+alert(typeof json);
 
-/*   尝试解答时的错误
+alert(json);
 
-let tempDate = date;  函数中的 tempDate 只是对 date 的引用，并不是一个新的对象。所以当你修改 tempDate 时，实际上是在修改 date。对象部分的知识
+/* 在JavaScript中，JSON（JavaScript Object Notation）格式确实是字符串格式。
 
-return tempDate.setDate(tempDate.getDate() + days);
-这样写的话返回的是时间戳而不是tempDate本身。因为setDate 方法返回的是更新后的时间的时间戳，而不是 Date 对象
- */
+JSON是一种轻量级的数据交换格式，易于人类读取和编写，也易于机器解析和生成。它的基本结构是由键值对组成的对象和有序列表组成的数组，符合JavaScript对象表示的语法，但JSON是以字符串的形式存在的。 */
 
-/*   答案解释
-
-思路很简单：从 date 中减去给定的天数：
-……但是函数不能修改 date。这一点很重要，因为我们提供日期的外部代码不希望它被修改。
-
-要实现这一点，我们可以复制这个日期，就像这样：
-*/
+/* JSON.stringify 将对象转换为 JSON。
+JSON.parse 将 JSON 转换回对象。 */
