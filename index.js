0cad74508f6170bf5f4ef38c6025ed9ef56f36bc
xiaoka <panjixiao@outlook.com>
2024-05-15 15:13:20 +0900

    使用 reviver

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-let userData = `{"name":"John","age":35,"isAdmin":false,"friends":[0,1,2,3]}`;
-
-let user = JSON.parse(userData);
-
-alert(user.friends[1]);
-
-/* 要解码 JSON 字符串，我们需要另一个方法 JSON.parse。
-
-语法：
-
-let value = JSON.parse(str, [reviver]); 
-
-str
-要解析的 JSON 字符串。
-reviver
-可选的函数 function(key,value)，该函数将为每个 (key, value) 对调用，并可以对值进行转换*/
-
-
-/* SON 可能会非常复杂，对象和数组可以包含其他对象和数组。但是它们必须遵循相同的 JSON 格式。
-
-以下是手写 JSON 时的典型错误（有时我们必须出于调试目的编写它）：
-
-let json = `{
-  name: "John",                     // 错误：属性名没有双引号
-  "surname": 'Smith',               // 错误：值使用的是单引号（必须使用双引号）
-  'isAdmin': false                  // 错误：键使用的是单引号（必须使用双引号）
-  "birthday": new Date(2000, 2, 3), // 错误：不允许使用 "new"，只能是裸值
-  "friends": [0,1,2,3]              // 这个没问题
+let schedule = `{
+  "meetups": [
+    {"title":"Conference","date":"2017-11-30T12:00:00.000Z"},
+    {"title":"Birthday","date":"2017-04-18T12:00:00.000Z"}
+  ]
 }`;
+
+schedule = JSON.parse(schedule, function (key, value) {
+  if (key == 'date') return new Date(value);
+  return value;
+});
+
+alert(schedule.meetups[1].date.getDate());
+
+/* 
+meetup.date 的值是一个字符串，而不是 Date 对象。JSON.parse 怎么知道应该将字符串转换为 Date 呢？
+
+让我们将 reviver 函数传递给 JSON.parse 作为第二个参数，该函数按照“原样”返回所有值，但是 date 会变成 Date
 */
