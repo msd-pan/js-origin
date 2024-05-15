81126140507229280a8373741fb59709d2308e47
xiaoka <panjixiao@outlook.com>
2024-05-15 15:46:09 +0900

    (练习)排除反向引用

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-let user = {
-  name: 'John Smith',
-  age: 35,
+let room = { number: 23 };
+
+let meetup = {
+  title: 'Conference',
+  occupiedBy: [{ name: 'John' }, { name: 'Alice' }],
+  place: room,
 };
 
-let newUser = JSON.parse(JSON.stringify(user));
-alert(newUser);
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+alert(
+  JSON.stringify(meetup, function replacer(key, value) {
+    return key != '' && value == meetup ? undefined : value;
+  }),
+);
+
+/*
+{
+  "title":"Conference",
+  "occupiedBy":[{"name":"John"},{"name":"Alice"}],
+  "place":{"number":23}
+}
+
+这里我们还需要判断 key=="" 以排除第一个调用时 value 是 meetup 的情况。
+*/
