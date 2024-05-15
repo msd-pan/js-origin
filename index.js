047dfcab258f876b7971197754ced6b875d77ca2
xiaoka <panjixiao@outlook.com>
2024-05-15 14:51:29 +0900

    JSON.parse

--- index.js
+++ index.js
@@ -1,41 +1,30 @@
-let room = { number: 23 };
+let userData = `{"name":"John","age":35,"isAdmin":false,"friends":[0,1,2,3]}`;
 
-let meetup = {
-  title: 'Conference',
-  date: new Date(Date.UTC(2017, 0, 1)),
-  room,
-};
+let user = JSON.parse(userData);
 
-alert(JSON.stringify(meetup));
+alert(user.friends[1]);
 
-/*
-  {
-    "title":"Conference",
-    "date":"2017-01-01T00:00:00.000Z",  // (1)
-    "room": {"number":23}               // (2)
-  }
+/* 要解码 JSON 字符串，我们需要另一个方法 JSON.parse。
+
+语法：
+
+let value = JSON.parse(str, [reviver]); 
+
+str
+要解析的 JSON 字符串。
+reviver
+可选的函数 function(key,value)，该函数将为每个 (key, value) 对调用，并可以对值进行转换*/
+
+
+/* SON 可能会非常复杂，对象和数组可以包含其他对象和数组。但是它们必须遵循相同的 JSON 格式。
+
+以下是手写 JSON 时的典型错误（有时我们必须出于调试目的编写它）：
+
+let json = `{
+  name: "John",                     // 错误：属性名没有双引号
+  "surname": 'Smith',               // 错误：值使用的是单引号（必须使用双引号）
+  'isAdmin': false                  // 错误：键使用的是单引号（必须使用双引号）
+  "birthday": new Date(2000, 2, 3), // 错误：不允许使用 "new"，只能是裸值
+  "friends": [0,1,2,3]              // 这个没问题
+}`;
 */
-
-/* 像 toString 进行字符串转换，对象也可以提供 toJSON 方法来进行 JSON 转换。如果可用，JSON.stringify 会自动调用它。 
-
-在这儿我们可以看到 date (1) 变成了一个字符串。这是因为所有日期都有一个内建的 toJSON 方法来返回这种类型的字符串。
-
-现在让我们为对象 room 添加一个自定义的 toJSON：*/
-
-let room1 = {
-  number: 23,
-  toJSON() {
-    return this.number;
-  },
-};
-
-let meetup1 = {
-  title: 'Conference',
-  room1,
-};
-
-alert(JSON.stringify(room1));
-
-alert(JSON.stringify(meetup1));
-
-/* 正如我们所看到的，toJSON 既可以用于直接调用 JSON.stringify(room) 也可以用于当 room 嵌套在另一个编码对象中时。 */
