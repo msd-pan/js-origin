06aa890ba489a2a8cc560302cee646b137b872ca
xiaoka <panjixiao@outlook.com>
2024-05-16 14:19:13 +0900

    递归结构,链表

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-let company = {
-  sales: [
-    { name: 'John', salary: 1000 },
-    { name: 'Alice', salary: 1600 },
-  ],
-  development: {
-    sites: [
-      { name: 'Peter', salary: 2000 },
-      { name: 'Alex', salary: 1800 },
-    ],
-    internals: [{ name: 'JAck', salary: 1300 }],
-  },
-};
+let list = { value: 1 };
+list.next = { value: 2 };
+list.next = { value: 3 };
+list.next = { value: 4 };
+ist.next = { value: 5 };
 
-// 用来完成任务的函数，求和函数
-let sumSalaries = (department) => {
-  if (Array.isArray(department)) {
-    //判断是否为数组的方法，返回布尔值
-    return department.reduce((prev, current) => prev + current.salary, 0); // 遍历数组时使用的arr.reduce 方法（https://zh.javascript.info/array-methods）
-  } else {
-    let sum = 0;
-    for (let subep of Object.values(department)) {
-      sum += sumSalaries(subep);
-    }
-    return sum;
-  }
-};
+// 将新值添加到链表头部
+list = { value: 'new item', next: list };
 
-alert(sumSalaries(company));
+// 要从中间删除一个值，可以修改前一个元素的 next：
+// list.next = list.next.next;
