88b2ad2285e214a97b6af62104f31a4f6fc49187
xiaoka <panjixiao@outlook.com>
2024-05-16 15:53:39 +0900

    复制 array/object

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-let arr1 = [1, -2, 3, 4];
-let arr2 = [8, 3, 4, -2];
+let arr = [1, 2, 3];
 
-let merged = [0, ...arr1, 1, ...arr2];
+let arrCopy = [...arr]; // 将数组spread到参数列表中，然后结果放到一个新数组中
 
-alert(Math.max(1, ...arr1, 2, ...arr2, 25));
+// 判断两个数组中的内容相同吗，相同
+alert(JSON.stringify(arr) === JSON.stringify(arrCopy));
 
-let str = 'Hello';
+// 判断两个数组相等吗？ 不相等
+alert(arr === arrCopy); // 两个数组的引用是不同的，故不相等
 
-alert([...str]);
+// 修改我们初始的数组不会修改副本
+arr.push(4);
+alert(arr); // 1，2，3，4
+alert(arrCopy); // 1，2，3
 
-/* 不过 Array.from(obj) 和 [...obj] 存在一个细微的差别：
+let obj = { a: 1, b: 3, c: 4 };
 
-Array.from 适用于类数组对象也适用于可迭代对象。
-Spread 语法只适用于可迭代对象。
-因此，对于将一些“东西”转换为数组的任务，Array.from 往往更通用。 */
+let objCopy = { ...obj };
 
-/* Spread 语法内部使用了迭代器来收集元素，与 for..of 的方式相同。它看起来和 rest 参数很像，也使用 ...，但是二者的用途完全相反。
+alert(JSON.stringify(obj) === JSON.stringify(objCopy));
 
-当在函数调用中使用 ...arr 时，它会把可迭代对象 arr “展开”到参数列表中。 */
+alert(obj === objCopy);
+
+// 修改我们初始的对象不会修改副本：
+obj.d = 4;
+alert(JSON.stringify(obj)); // {"a":1,"b":2,"c":3,"d":4}
+alert(JSON.stringify(objCopy)); // {"a":1,"b":2,"c":3}
+
+/*  
+使用 spread 语法可以进行浅拷贝
+
+这种方式比使用 let arrCopy = Object.assign([], arr) 复制数组，或使用 let objCopy = Object.assign({}, obj) 复制对象来说更为简便。因此，只要情况允许，我们倾向于使用它。
+*/
