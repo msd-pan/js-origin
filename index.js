770228e546a9337baeca517d18558821f9e6711d
xiaoka <panjixiao@outlook.com>
2024-05-16 13:34:13 +0900

    递归和堆栈: 递归遍历

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-let room = { number: 23 };
-
-let meetup = {
-  title: 'Conference',
-  occupiedBy: [{ name: 'John' }, { name: 'Alice' }],
-  place: room,
+let company = {
+  sales: [
+    { name: 'John', salary: 1000 },
+    { name: 'Alice', salary: 1600 },
+  ],
+  development: {
+    sites: [
+      { name: 'Peter', salary: 2000 },
+      { name: 'Alex', salary: 1800 },
+    ],
+    internals: [{ name: 'JAck', salary: 1300 }],
+  },
 };
 
-room.occupiedBy = meetup;
-meetup.self = meetup;
+// 用来完成任务的函数，求和函数
+let sumSalaries = (department) => {
+  if (Array.isArray(department)) {
+    //判断是否为数组的方法，返回布尔值
+    return department.reduce((prev, current) => prev + current.salary, 0); // 遍历数组时使用的arr.reduce 方法（https://zh.javascript.info/array-methods）
+  } else {
+    let sum = 0;
+    for (let subep of Object.values(department)) {
+      sum += sumSalaries(subep);
+    }
+    return sum;
+  }
+};
 
-alert(
-  JSON.stringify(meetup, function replacer(key, value) {
-    return key != '' && value == meetup ? undefined : value;
-  }),
-);
-
-/*
-{
-  "title":"Conference",
-  "occupiedBy":[{"name":"John"},{"name":"Alice"}],
-  "place":{"number":23}
-}
-
-这里我们还需要判断 key=="" 以排除第一个调用时 value 是 meetup 的情况。
-*/
+alert(sumSalaries(company));
