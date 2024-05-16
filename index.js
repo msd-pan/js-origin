a1b0c612122b2b49f7168a652632e9c6447b3c8d
xiaoka <panjixiao@outlook.com>
2024-05-16 14:42:34 +0900

    “arguments” 变量

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-let showName = (firstName, lastName, ...titles) => {
-  alert(firstName + '' + lastName);
+function showName() {
+  alert(arguments.length);
+  alert(arguments[0]);
+  alert(arguments[1]);
 
-  // 剩余的参数将被放入titles数组中
-  alert(titles[0]);
-  alert(titles[1]);
-  alert(titles.length);
-};
+  // 它是可遍历的
+  // for(let arg of arguments) alert(arg);
+}
 
-showName('Julius', 'Caesar', 'Consul', 'Imperator');
+// 依次显示：2，Julius，Caesar
+showName('Julius', 'Caesar');
 
-/* 我们也可以选择将第一个参数获取为变量，并将剩余的参数收集起来。
+// 依次显示：1，Ilya，undefined（没有第二个参数）
+showName('Ilya');
 
-下面的例子把前两个参数获取为变量，并把剩余的参数收集到 titles 数组中： */
+/* 
+在过去，JavaScript 中不支持 rest 参数语法，而使用 arguments 是获取函数所有参数的唯一方法。现在它仍然有效，我们可以在一些老代码里找到它。
+
+但缺点是，尽管 arguments 是一个类数组，也是可迭代对象，但它终究不是数组。它不支持数组方法，因此我们不能调用 arguments.map(...) 等方法。
+
+此外，它始终包含所有参数，我们不能像使用 rest 参数那样只截取参数的一部分。
+
+因此，当我们需要这些功能时，最好使用 rest 参数。
+
+
+箭头函数没有自身的 this。现在我们知道了它们也没有特殊的 arguments 对象
+*/
